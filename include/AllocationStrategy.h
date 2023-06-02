775b214d3098973c7f601c4fe556c42339b5b11d
MONNIOT Julien <julien.monniot@inria.fr>
2023-06-02 18:13:34 +0200

    Refactored AllocationStrategy (Lustre part) into smaller functions

--- include/AllocationStrategy.h
+++ include/AllocationStrategy.h
@@ -13,6 +13,7 @@ namespace storalloc {
         const std::map<std::shared_ptr<wrench::DataFile>, std::vector<std::shared_ptr<wrench::FileLocation>>>& mapping,
         const std::vector<std::shared_ptr<wrench::FileLocation>>& previous_allocations);
 
+
     /** @brief  Main entry point for the implementation of Lustre allocation strategy
      *          which redirects either to the Round-Robin allocator (lustreRRStrategy) 
      *          or the weighted allocator (lustreWeightedStrategy)
@@ -39,6 +40,10 @@ namespace storalloc {
         const std::vector<std::shared_ptr<wrench::FileLocation>>& previous_allocations);
 
 
+    /** 
+     *  CONST FOR LUSTRE STRATEGIES
+    */
+
     // "Magic" default Lustre settings, that equals to computing whether or not the free space
     // diff between OSTs is greater thant 17% or not
     const auto LUSTRE_lq_threshold_rr = 43;
@@ -47,15 +52,41 @@ namespace storalloc {
     // Here we use the default priority, which balanced towards free space (91%)
     const auto LUSTRE_lq_prio_free = 232;
     const auto LUSTRE_prio_wide = 256;
+    const auto LUSTRE_max_nb_ost = 2000;    // never stripe on more than 2000 OSTs, that's the Lustre limit when using ZFS.
+         
+    /** Currently, the stripe size is arbitrarily set to 512MB (recommended size is between 1-4MB, and max is 4GB, but anyway, our allocations
+     *  do not necessarily represent files...)
+     */
+    const double LUSTRE_stripe_size = 512000000;    // how much data is written to a given OST before moving to the next one (512MB in this case)
+    
+
+    /** 
+     *  HELPER FUNCTION FOR LUSTRE STRATEGIES
+    */
 
     struct ba_min_max {
         uint64_t min;
         uint64_t max;
     };
 
-    ba_min_max compute_min_max_utilization(const std::map<std::string, std::vector<std::shared_ptr<wrench::StorageService>>>&); 
+    ba_min_max lustreComputeMinMaxUtilization(const std::map<std::string, std::vector<std::shared_ptr<wrench::StorageService>>>&); 
 
-    bool lustre_use_rr(struct ba_min_max ba_min_max);
+    bool lustreUseRR(struct ba_min_max ba_min_max);
+
+    std::vector<std::shared_ptr<wrench::StorageService>> lustreRROrderServices(const std::map<std::string, int> &hostname_to_service_count, 
+                                                                           const std::vector<std::shared_ptr<wrench::StorageService>>& disk_level_services);
+
+    unsigned int lustreComputeStripesPerOST(double file_size_b, double stripe_size, size_t number_of_OSTs, double stripe_count);
+
+    bool lustreOstIsUsed(const std::map<std::shared_ptr<wrench::DataFile>, std::vector<std::shared_ptr<wrench::FileLocation>>>& mapping, const std::shared_ptr<wrench::StorageService> ost);
+
+    std::vector<std::shared_ptr<wrench::FileLocation>> lustreCreateFileParts(const std::string &file_id, std::map<int, std::shared_ptr<wrench::StorageService>> temp_allocations);
+
+    uint64_t lustreComputeOstPenalty(uint64_t free_space_b, uint64_t free_inode_count, double active_service_count);
+
+    uint64_t lustreComputeOstWeight(uint64_t free_space_b, uint64_t free_inode_count, uint64_t ost_penalty);
+
+    uint64_t lustreComputeOssPenalty(uint64_t free_space_b, uint64_t free_inode_count, size_t ost_count, size_t oss_count);
 
 } // storalloc
 
