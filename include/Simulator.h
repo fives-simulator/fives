a15165c829eeb9a4143e4811de454669f9337dce
MONNIOT Julien <julien.monniot@inria.fr>
2023-06-07 17:29:29 +0200

    More functional tests and refactoring in Simulator.cpp ; first skeleton for 'advanced' tests of Lustre allocation helpers

--- include/Simulator.h
+++ include/Simulator.h
@@ -1,9 +1,35 @@
 #ifndef SIMULATOR_H
 #define SIMULATOR_H
 
+#include <set>
+#include <memory>
+#include <wrench-dev.h>
 
-namespace storalloc {
+#include "ConfigDefinition.h"
 
+namespace storalloc
+{
+
+    /**
+     * @brief Helper function to instatiate all required StorageServices from the main config
+     * @param simulation An initialized Wrench simulation object
+     * @param config The configuration loaded from YAML
+     * @return Set of instantiated storage services
+     */
+    std::set<std::shared_ptr<wrench::StorageService>> instantiateStorageServices(std::shared_ptr<wrench::Simulation> simulation,
+                                                                                 std::shared_ptr<storalloc::Config> config);
+
+  
+    /** 
+     * @brief Helper function to instatiate all required ComputeServices from the main config
+     * @param simulation An initialized Wrench simulation object
+     * @param config The configuration loaded from YAML
+     * @return Resulting wrench BatchComputeService 
+     */
+    std::shared_ptr<wrench::BatchComputeService>  instantiateComputeServices(std::shared_ptr<wrench::Simulation> simulation, 
+                                                      std::shared_ptr<storalloc::Config> config);
+
+  
     /**
      * @brief The Simulator's main function
      *
@@ -12,7 +38,7 @@ namespace storalloc {
      * @return 0 on success, non-zero otherwise
      */
     int run_simulation(int argc, char **argv);
-    
+
 } // storalloc
 
 #endif // SIMULATOR_H
--- test/FunctionalTest.cpp
+++ test/FunctionalTest.cpp
@@ -6,39 +6,39 @@
 #include "../include/Utils.h"
 #include "../include/Platform.h"
 
-
-class BasicFunctionalTest : public ::testing::Test {
+class BasicFunctionalTest : public ::testing::Test
+{
 
 public:
-
     void create_platform_test();
     void do_Functional_test();
+    void instantiate_storage_services_test();
+    void instantiate_compute_services_test();
 
 protected:
     ~BasicFunctionalTest() {}
 
     BasicFunctionalTest() {}
-
 };
-
 
 /**********************************************************************/
 /**  Functionnal Test 1                                              **/
 /**********************************************************************/
 
-TEST_F(BasicFunctionalTest, BasicFunctionalTest1) {
+TEST_F(BasicFunctionalTest, BasicFunctionalTest1)
+{
     // DO_TEST_WITH_FORK(do_Functional_test);
 }
 
-
 /**
- *  @brief Basic functional test (just running a simple configuration and job file scenario), 
+ *  @brief Basic functional test (just running a simple configuration and job file scenario),
  *         not checking anything except the execution without exceptions.
  */
-void BasicFunctionalTest::do_Functional_test() {
+void BasicFunctionalTest::do_Functional_test()
+{
 
     int argc = 3;
-    char **argv = (char **) calloc(argc, sizeof(char *));
+    char **argv = (char **)calloc(argc, sizeof(char *));
     argv[0] = strdup("unit_test");
     argv[1] = strdup("../configs/test_config.yml");
     argv[2] = strdup("../data/IOJobsTest_1.yml");
@@ -50,7 +50,6 @@ void BasicFunctionalTest::do_Functional_test() {
     free(argv);
 }
 
-
 TEST_F(BasicFunctionalTest, create_platform_test)
 {
     DO_TEST_WITH_FORK(create_platform_test);
@@ -59,28 +58,29 @@ TEST_F(BasicFunctionalTest, create_platform_test)
 /**
  *  @brief Testing programmatic platform creation
  */
-void BasicFunctionalTest::create_platform_test() {
+void BasicFunctionalTest::create_platform_test()
+{
 
     auto config = std::make_shared<storalloc::Config>(storalloc::loadConfig("../configs/test_config.yml"));
 
     auto simulation = wrench::Simulation::createSimulation();
     int argc = 1;
-    char **argv = (char **) calloc(argc, sizeof(char *));
+    char **argv = (char **)calloc(argc, sizeof(char *));
     argv[0] = strdup("unit_test");
     ASSERT_NO_THROW(simulation->init(&argc, argv));
-    
+
     auto platform_factory = storalloc::PlatformFactory(config);
     simulation->instantiatePlatform(platform_factory);
-    
+
     // Check compute nodes topo
     auto all_hosts_by_cluster = simulation->getHostnameListByCluster();
     ASSERT_NE(all_hosts_by_cluster.find("AS_DragonflyCompute"), all_hosts_by_cluster.end());
     ASSERT_EQ(all_hosts_by_cluster["AS_DragonflyCompute"].size(), 384);
-    ASSERT_TRUE(simulation->doesHostExist("compute0")); // first
+    ASSERT_TRUE(simulation->doesHostExist("compute0"));   // first
     ASSERT_TRUE(simulation->doesHostExist("compute383")); // last compute node
 
     ASSERT_EQ(wrench::Simulation::getHostNumCores("compute0"), 64);
-    ASSERT_EQ(wrench::S4U_Simulation::getHostMemoryCapacity("compute78"), 192000000000);    // In bytes
+    ASSERT_EQ(wrench::S4U_Simulation::getHostMemoryCapacity("compute78"), 192000000000); // In bytes
 
     // Check other zones topo
     auto all_hosts_by_zone = wrench::S4U_Simulation::getAllHostnamesByZone();
@@ -109,24 +109,39 @@ void BasicFunctionalTest::create_platform_test() {
     ASSERT_EQ(wrench::S4U_Simulation::getDiskCapacity("capacity1", "/dev/hdd0"), 200000000000); // in bytes
     ASSERT_EQ(wrench::S4U_Simulation::getDiskCapacity("capacity1", "/dev/hdd1"), 200000000000);
     ASSERT_EQ(wrench::S4U_Simulation::getDiskCapacity("capacity1", "/dev/ssd0"), 96000000000);
-    
+
     auto capacity2 = wrench::S4U_Simulation::get_host_or_vm_by_name("capacity2");
     ASSERT_EQ(capacity2->get_disks()[0]->get_sharing_policy(sg4::Disk::Operation::READ), sg4::Disk::SharingPolicy::NONLINEAR);
     ASSERT_EQ(capacity2->get_disks()[0]->get_sharing_policy(sg4::Disk::Operation::WRITE), sg4::Disk::SharingPolicy::NONLINEAR);
 
-    for (const auto disk : capacity2->get_disks()) {
-        if ((disk->get_name() == "hdd_capa0") || (disk->get_name() == "hdd_capa1")) {
+    for (const auto disk : capacity2->get_disks())
+    {
+        if ((disk->get_name() == "hdd_capa0") || (disk->get_name() == "hdd_capa1"))
+        {
             ASSERT_EQ(disk->get_read_bandwidth(), 100000000);
             ASSERT_EQ(disk->get_write_bandwidth(), 50000000);
-            //ASSERT_EQ(disk->get_concurrency_limit(), 500);
-        } else if (disk->get_name() == "ssd_perf0") {
+            // ASSERT_EQ(disk->get_concurrency_limit(), 500);
+        }
+        else if (disk->get_name() == "ssd_perf0")
+        {
             ASSERT_EQ(disk->get_read_bandwidth(), 1000000000);
             ASSERT_EQ(disk->get_write_bandwidth(), 500000000);
             // ASSERT_EQ(disk->get_concurrency_limit(), -1);
-        } else {
-            GTEST_FAIL_AT(__FILE__, __LINE__);
+        }
+        else
+        {
+            GTEST_FAIL();
         }
     }
+
+    // Static "permanent storage" node
+    auto permanent = wrench::S4U_Simulation::get_host_or_vm_by_name("permanent_storage");
+    ASSERT_EQ(permanent->get_disks()[0]->get_read_bandwidth(), 10000000000);
+    ASSERT_EQ(permanent->get_disks()[0]->get_write_bandwidth(), 10000000000);
+    auto perm_disk_properties = permanent->get_disks()[0]->get_properties();
+    ASSERT_EQ(perm_disk_properties->at("size"), "10000TB");
+    ASSERT_EQ(wrench::S4U_Simulation::getDiskCapacity("permanent_storage", "/dev/disk0"), 10000000000000000);
+    ASSERT_EQ(perm_disk_properties->at("mount"), "/dev/disk0");
 
     // Check links
     auto link_names = wrench::S4U_Simulation::getAllLinknames();
@@ -144,40 +159,117 @@ void BasicFunctionalTest::create_platform_test() {
     ASSERT_TRUE(simulation->doesLinkExist("capacity3_DOWN"));
     ASSERT_TRUE(simulation->doesLinkExist("capacity3_UP"));
     ASSERT_TRUE(simulation->doesLinkExist("permanent_storage_DOWN"));
-    ASSERT_TRUE(simulation->doesLinkExist("permanent_storage_UP"));    
+    ASSERT_TRUE(simulation->doesLinkExist("permanent_storage_UP"));
     ASSERT_TRUE(simulation->doesLinkExist("compound_storage_DOWN"));
     ASSERT_TRUE(simulation->doesLinkExist("compound_storage_UP"));
 
-
-    /*
-    std::string start, end;
-    start = "compute34";
-    end = "capacity2";
-    auto route = wrench::Simulation::getRoute(start, end);
-    for (const auto &hop : route) {
-        std::cout << " - " << hop << std::endl;
-    }
-
-    start = "user0";
-    end = "capacity2";
-    auto route2 = wrench::Simulation::getRoute(start, end);
-    for (const auto &hop : route2) {
-        std::cout << " - " << hop << std::endl;
-    }
-
-    
-    auto compute14 = wrench::S4U_Simulation::get_host_or_vm_by_name("compute14");
-        auto compute101 = wrench::S4U_Simulation::get_host_or_vm_by_name("compute101");
-    auto user0 = wrench::S4U_Simulation::get_host_or_vm_by_name("user0");
-    std::vector<simgrid::s4u::Link*> linksInRoute;
-    double latency = 0;
-    //std::unordered_set<simgrid::kernel::routing::NetZoneImpl*> netzonesInRoute;
-    user0->route_to(compute14, linksInRoute, &latency);
-
-    auto compute_zone = compute101->get_englobing_zone();
-    auto properties = capacity2->get_properties();
-    */
-
     // auto s1 = wrench::SimpleStorageService::createSimpleStorageService("capacity1", {"/dev/hdd1"});
+}
 
+TEST_F(BasicFunctionalTest, instantiate_storage_services_test)
+{
+    DO_TEST_WITH_FORK(instantiate_storage_services_test);
+}
+
+/**
+ *  @brief Testing programmatic platform creation
+ */
+void BasicFunctionalTest::instantiate_storage_services_test()
+{
+
+    auto config = std::make_shared<storalloc::Config>(storalloc::loadConfig("../configs/test_config.yml"));
+
+    auto simulation = wrench::Simulation::createSimulation();
+    int argc = 1;
+    char **argv = (char **)calloc(argc, sizeof(char *));
+    argv[0] = strdup("unit_test");
+    ASSERT_NO_THROW(simulation->init(&argc, argv));
+
+    auto platform_factory = storalloc::PlatformFactory(config);
+    simulation->instantiatePlatform(platform_factory);
+
+    auto ss_storages = storalloc::instantiateStorageServices(simulation, config);
+
+    ASSERT_EQ(ss_storages.size(), 12); // Four storage nodes * 3 disks = 12 storage services
+
+    for (const auto &service : ss_storages)
+    {
+        auto simple = std::dynamic_pointer_cast<wrench::SimpleStorageService>(service);
+        auto mount_point = simple->getMountPoints();
+        ASSERT_EQ(mount_point.size(), 1);
+        if ((mount_point.find("/dev/hdd0") != mount_point.end()) || (mount_point.find("/dev/hdd1") != mount_point.end()))
+        {
+            ASSERT_EQ(simple->traceTotalFreeSpace(), 200000000000);
+            ASSERT_EQ(simple->traceTotalFiles(), 0);
+        }
+        else if (mount_point.find("/dev/ssd0") != mount_point.end())
+        {
+            ASSERT_EQ(simple->traceTotalFreeSpace(), 96000000000);
+            ASSERT_EQ(simple->traceTotalFiles(), 0);
+        }
+        else
+        {
+            GTEST_FAIL();
+        }
+    }
+}
+
+class TestController : public wrench::ExecutionController
+{
+public:
+    TestController(const std::shared_ptr<wrench::ComputeService> &compute_service,
+                   const std::string &hostname) : wrench::ExecutionController(hostname, "controller"), compute_s(compute_service) {}
+
+    int main() override
+    {
+        
+        if (this->compute_s->getNumHosts() != 384) {
+            throw std::runtime_error("Invalid number of compute hosts");
+        }
+
+        if (this->compute_s->getTotalNumCores() != 384 * 64) {
+            throw std::runtime_error("Invalid core number");
+        }
+
+        for (const auto &mem : this->compute_s->getMemoryCapacity()) {
+            if (mem.second != 192000000000) {
+                throw std::runtime_error("Invalid RAM capcity "+ std::to_string(mem.second));
+            }
+        }
+    
+        return 0;
+    }
+
+    std::shared_ptr<wrench::ComputeService> compute_s;
+
+};
+
+TEST_F(BasicFunctionalTest, instantiate_compute_services_test)
+{
+    DO_TEST_WITH_FORK(instantiate_compute_services_test);
+}
+
+/**
+ *  @brief Testing programmatic platform creation
+ */
+void BasicFunctionalTest::instantiate_compute_services_test()
+{
+
+    auto config = std::make_shared<storalloc::Config>(storalloc::loadConfig("../configs/test_config.yml"));
+
+    auto simulation = wrench::Simulation::createSimulation();
+    int argc = 1;
+    char **argv = (char **)calloc(argc, sizeof(char *));
+    argv[0] = strdup("unit_test");
+    ASSERT_NO_THROW(simulation->init(&argc, argv));
+
+    auto platform_factory = storalloc::PlatformFactory(config);
+    simulation->instantiatePlatform(platform_factory);
+
+    auto batch_service = storalloc::instantiateComputeServices(simulation, config);
+
+    auto wms = simulation->add(
+        new TestController(batch_service, "user0"));
+
+    ASSERT_NO_THROW(simulation->launch());
 }
